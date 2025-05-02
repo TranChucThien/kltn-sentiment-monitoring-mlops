0b218d51a337c8414f2e61577e78b86820c66add
TranChucThien <chucthien2@gmail.com>
2025-05-02 23:28:00 +0700

    fix pipeline eval to parallel

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import argparse
 from src._01_dataset_preparation import main as dataset_main
 from src._02_training import main as training_main
 from src._03_evaluate import main as evaluate_main
+import threading
 
 def run_dataset():
     print("📦 Running Dataset Preparation...")
@@ -18,15 +19,32 @@ def run_training():
     cv_version, tf_version = training_main()
     print(f"✅ Training done: CV v{cv_version}, TF v{tf_version}")
 
+def run_evaluate_count_vector():
+    print("📊 Running Evaluation for CountVecorizer Model...")
+    # cv_version, tf_version = training_main()  # hoặc load version từ file
+    evaluate_main(name="CountVectorizer_Model", input_data=False)
+    # evaluate_main(name="HashingTF_IDF_Model", input_data=False)
+
+def run_evaluate_hashing_tf():
+    print("📊 Running Evaluation for HashingTF Model...")
+    # cv_version, tf_version = training_main()  # hoặc load version từ file
+    evaluate_main(name="HashingTF_IDF_Model", input_data=False)
 def run_evaluate():
     print("📊 Running Evaluation...")
     # cv_version, tf_version = training_main()  # hoặc load version từ file
-    evaluate_main(name="CountVectorizer_Model", input_data=False)
-    evaluate_main(name="HashingTF_IDF_Model", input_data=False)
+    run_evaluate_count_vector()
+    run_evaluate_hashing_tf()
+    thread1 = threading.Thread(target=run_evaluate_count_vector)
+    thread2 = threading.Thread(target=run_evaluate_hashing_tf)
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+
 
 def main():
     parser = argparse.ArgumentParser(description="ML Pipeline CLI")
-    parser.add_argument("step", choices=["all", "dataset", "train", "eval"],
+    parser.add_argument("step", choices=["all", "dataset", "train", "eval", "eval_count_vector", "eval_hashing_tf"],
                         help="Which step to run")
 
     args = parser.parse_args()
@@ -34,14 +52,22 @@ def main():
     if args.step == "all":
         run_dataset()
         run_training()
-        run_evaluate()
+        thread1 = threading.Thread(target=run_evaluate_count_vector)
+        thread2 = threading.Thread(target=run_evaluate_hashing_tf)
+        thread1.start()
+        thread2.start()
+        thread1.join()
+        thread2.join()
     elif args.step == "dataset":
         run_dataset()
     elif args.step == "train":
         run_training()
+    elif args.step == "eval_count_vector":
+        run_evaluate_count_vector()
+    elif args.step == "eval_hashing_tf":
+        run_evaluate_hashing_tf()
     elif args.step == "eval":
         run_evaluate()
-
         
 
 if __name__ == "__main__":
